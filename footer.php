f3fca2c0841bcc4c953e70fd29dd4d1bfed469b9
vpsrei <tr_shopping@yahoo.co.jp>
2018-07-01 00:16:59 +0900

    change request forum design

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 <div id="footerBottom">
 	<?php wp_nav_menu( array( 'container_id' => 'footer-menu', 'theme_location' => 'footer-menu', 'depth' => 1, 'fallback_cb' => 0 ) ); ?>
 	<div id="copyright"><!-- コピーライト -->
-	<?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?> 
+	Ⓒ <?php echo date('Y'); ?> <?php bloginfo('name'); ?> 
 	掲載画像の無断転載は固くお断り致します。
 <!-- 	WordPress CMS Theme <a href="http://wsc.studiobrain.net/" target="_blank">WSC Project</a>. -->
 	</div><!-- /コピーライト -->
